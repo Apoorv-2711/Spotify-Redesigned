439219a00fd66bca88ffcaa485c387b6dccf4b36
Apoorv-2711 <apoorvjain162@gmail.com>
2023-04-21 17:31:52 +0530

    initial commit

---
+++ components/Dashboard.js
@@ -0,0 +1,14 @@
+import Leftbar from "./Leftbar";
+import Body from "./Body";
+import Rightbar from "./Rightbar";
+
+export const Dashboard = () => {
+    return (
+        <main>
+            <Leftbar />
+            <Body />
+            <Rightbar />
+        </main>
+    )
+}
+
---
+++ components/Leftbar.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+function Leftbar() {
+  return (
+    <section className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8 ">
+        <Image src="https://media.discordapp.net/attachments/898544585167482891/900083852658225172/image0.png?width=619&height=637" width={56} height={56} />
+        <div>
+
+        </div>
+
+
+
+    </section>
+  )
+}
+
+export default Leftbar;
