624af29ded6229cc9a6b7addb929f36ca30dc050
Apoorv-2711 <apoorvjain162@gmail.com>
2023-04-22 05:30:56 +0530

    Search bar and poster implementation

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Search from "./Search";
 import { useSession } from "next-auth/react";
+import Poster from "./Poster";
 
-function Body({ spotifyApi }) {
+function Body({ spotifyApi, chooseTrack }) {
     const { data: session } = useSession();
     const { accessToken } = session;
     const [search, setSearch] = useState("");
@@ -54,12 +55,21 @@ function Body({ spotifyApi }) {
 
     console.log(searchResults)
 
-    return (<section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
-        <Search search={search} setSearch={setSearch} />
-        <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
-
-        </div>
-    </section>
+    return (
+        <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
+            <Search search={search} setSearch={setSearch} />
+            <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
+                {searchResults.length === 0 ? newReleases.slice(0,4).map((track) => (
+                    <Poster key={track.id} track={track} 
+                    chooseTrack={chooseTrack} 
+                    />
+                )): searchResults.slice(0,4).map((track) => (
+                    <Poster key={track.id} track={track} 
+                    chooseTrack={chooseTrack}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
 
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,16 +2,23 @@ import Leftbar from "./Leftbar";
 import Body from "./Body";
 import Rightbar from "./Rightbar";
 import SpotifyWebApi from "spotify-web-api-node";
+import { useRecoilState } from "recoil";
+import { playingTrackState } from "@/atoms/playerAtom";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
+
 export const Dashboard = () => {
+    const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+    const chooseTrack = (track) => {
+        setPlayingTrack(track);
+    }
     return (
         <main>
             <Leftbar />
-            <Body spotifyApi={ spotifyApi } />
+            <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
             <Rightbar />
         </main>
     )
